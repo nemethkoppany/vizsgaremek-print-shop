fecb05831a7a5900034a84d50a29e0d31e183b41
Patrik <patsboy06@gmail.com>
2025-12-01 21:34:18 +0100

    fooldal

---
+++ Frontend/print-shop/src/components/Intro.jsx
@@ -0,0 +1,15 @@
+export default function Intro() {
+  return (
+    <section className="intro">
+      <h2>Bemutatkozás</h2>
+      <p>
+        Célunk, kényelmes, egyszerű online nyomtatási és másolási környezetet
+        biztosítani minden megrendelőnek. Legyen szó iskolai jegyzetekről,
+        céges dokumentumokról vagy személyes poszterekről, naptárakról, nálunk
+        mindent online intézhet pár kattintással. Szolgáltatásainkat úgy
+        alakítottuk ki, hogy minél kevesebb idő alatt garantáltan kiváló
+        minőséget kapjon.
+      </p>
+    </section>
+  );
+}
