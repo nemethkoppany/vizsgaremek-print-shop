ce6d68833f1699274c46b81f15b9623ff0e56a3e
Patrik <patsboy06@gmail.com>
2026-01-15 21:44:42 +0100

    szolgaltatas

--- Frontend/print-shop/src/App.jsx
+++ Frontend/print-shop/src/App.jsx
@@ -3,17 +3,37 @@ import Navbar from "./components/Navbar.jsx";
 import Arlista from "./components/Arlista.jsx";
 import Login from "./components/Login.jsx";
 import Profile from "./components/Profile.jsx";
+import Services from "./components/Services.jsx";
+import Cart from "./components/Cart.jsx";
 import "./index.css";
 import { useState } from "react";
 
 export default function App() {
-  const [page, setPage] = useState("home")
+  const [page, setPage] = useState("home");
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => setCartItems((prev) => [item, ...prev]);
+  const removeFromCart = (id) => setCartItems((prev) => prev.filter((x) => x.id !== id));
 
   return (
     <div className="container">
-      <Navbar page={page} setPage={setPage}/>
+      <Navbar page={page} setPage={setPage} cartCount={cartItems.length} />
+
       {page === "home" && <Fooldal />}
       {page === "arlista" && <Arlista setPage={setPage} />}
+      {page === "services" && (
+        <Services
+          addToCart={addToCart}
+          goToCart={() => setPage("cart")}
+        />
+      )}
+      {page === "cart" && (
+        <Cart
+          cartItems={cartItems}
+          removeFromCart={removeFromCart}
+          goToServices={() => setPage("services")}
+        />
+      )}
       {page === "login" && <Login setPage={setPage} />}
       {page === "profile" && <Profile />}
     </div>
